d96a216a197be03a964a0181318df14122a838e7
zacaugustzac <zacaugustzac@gmail.com>
2021-05-22 02:14:05 +0800

    hello huaizhong1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return 'hello huai zhong'
+    return 'hello huai zhong1'
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True)
